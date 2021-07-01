9c3eac984f4126940045ed7e9e80b1b98cc7a378
karimabuseer <karimabuseer@gmail.com>
2021-07-01 12:31:14 +0100

    Added 2nd way of testing rand num

--- spec/dice_app_spec.rb
+++ spec/dice_app_spec.rb
@@ -9,12 +9,12 @@ describe Dice do
     it 'Returns a number' do
         expect(subject.roll).to be_instance_of(Integer)
     end
-    # it 'Gives a random number between 1 and 6' do
-    #     expect(1..6).to include(subject.roll)                                                    (1<=)
-    # end
   
     it 'Gives a random number between 1 and 6' do
         expect(subject.roll).to be_between(1, 6)
     end
 
+    it 'Gives a random number between 1 and 6' do
+        expect([1,2,3,4,5,6]).to include(subject.roll)
+    end
 end
